a76b1b48966ff7f4032b86d107c9db1079e9997f
sarangkamble <sarangkamble.1994@gmail.com>
2020-07-01 22:56:09 +0530

    3 files modified

--- src/app/date1/date1.component.ts
+++ src/app/date1/date1.component.ts
@@ -10,8 +10,23 @@ export class Date1Component implements OnInit {
   constructor() { }
 
   @Input('read-only') readonly: boolean;
-
+  @Input('date-format') dateformat: string;
+  @Input('disabled') disabled = false;
+  dateModel: any;
   ngOnInit() {
+    this.dateModel = '07/07/2020'
   }
 
+  onInputChange(event: any) {
+    // if (event.target.value.length > 0) {
+    this.dateModel = new Date(event.target.value);
+    // } else {
+    //   this.dateModel = '';
+    // }
+  }
+  // Set touched on blur
+  onBlur() {
+    //this.onTouchedCallback();
+    //this.onChangeCallback(this.dateModel);
+  }
 }
